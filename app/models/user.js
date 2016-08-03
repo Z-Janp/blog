e6a95ca4be7dc78e11b5b4647fdc27663a216174
Z-Janp <songyagao@outlook.com>
2016-08-03 16:50:46 +0800

    music

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,71 @@
-const mongoose = require('mongoose');
-const UserSchema = require('../schemas/user.js');
-const User = mongoose.model('User', UserSchema);
-module.exports = User;
+const User = require('../models/user.js');
+exports.login = (req, res) => {
+    const _user = req.body.user;
+    const name = _user.name;
+    const password = _user.password;
+    User.findOne({ name: name }, (err, user) => {
+        if (err) {
+            console.log(err);
+        }
+        if (!user) {
+            return res.json({ error: -1, data: '用户名或密码错误！' });
+        }
+        const isMatch = user.comparePassword(password);
+        if (isMatch) {
+            req.session.user = user;
+            return res.json({ success: 1, data: '登录成功！', user: user.name });
+        } else {
+            return res.json({ error: -1, data: '用户名或密码错误！' });
+        }
+    })
+};
+//logout
+exports.logout = (req, res) => {
+    delete req.session.user;
+    res.redirect('/');
+};
+exports.showLogin = (req, res) => {
+    res.render('login', {
+        title: '登录'
+    })
+};
+//midware for user
+exports.loginRequire = (req, res, next) => {
+    const user = req.session.user;
+    if (!user) {
+        if (req.path.indexOf('/admin/article/list') > -1) {
+            res.redirect('/login');
+        }
+        return res.json({ error: -2, data: '未登录，请登陆后操作!' });
+    }
+    next();
+};
+//midware for user
+exports.adminRequire = (req, res, next) => {
+    const user = req.session.user;
+    if (user.role <= 10) {
+        return res.redirect('/login');
+    }
+    next();
+};
+//注册
+exports.signup = (req, res) => {
+    const _user = req.body.user;//user对象 中间键bodyParser将数据格式化为对象
+    //req.param('user')  express的实现
+    User.findOne({ name: _user.name }, (err, user) => {
+        if (err) {
+            console.log(err);//404 500
+        }
+        if (user) {
+            res.redirect('/login');//用户名已存在
+        } else {
+            user = new User(_user);
+            user.save((err, user) => {
+                if (err) {
+                    console.log(err);
+                }
+                res.redirect('/');
+            })
+        }
+    })
+}
